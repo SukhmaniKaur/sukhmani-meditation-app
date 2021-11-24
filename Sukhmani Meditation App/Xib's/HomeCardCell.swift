dbb6a5c811c8aec6996e2d89b61d2b9c55630c86
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-25 00:18:15 +0530

    designed home card cell

---
+++ Sukhmani Meditation App/Xib's/HomeCardCell.swift
@@ -0,0 +1,25 @@
+//
+//  HomeCardCell.swift
+//  Sukhmani Meditation App
+//
+//  Created by MACBOOK on 25/11/21.
+//
+
+import UIKit
+
+class HomeCardCell: UITableViewCell {
+
+    // OUTLETS
+    @IBOutlet weak var meditationNameLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
