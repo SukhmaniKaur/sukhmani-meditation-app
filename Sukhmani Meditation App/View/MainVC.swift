c4444599daa67e3b3ecc23f4cebbb267343a751f
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-25 00:30:15 +0530

    table view methods added to main vc

--- Sukhmani Meditation App/View/MainVC.swift
+++ Sukhmani Meditation App/View/MainVC.swift
@@ -13,8 +13,36 @@ class MainVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configUI()
     }
+    
+    //MARK: - configUI
+    private func configUI() {
+        tableView.register(UINib(nibName: TABLE_VIEW_CELL.HomeCardCell.rawValue, bundle: nil), forCellReuseIdentifier: TABLE_VIEW_CELL.HomeCardCell.rawValue)
+    }
+}
 
+//MARK: - UITableView DataSource and Delegate Methods
+extension MainVC: UITableViewDelegate, UITableViewDataSource {
+    
+    //numberOfRowsInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // heightForRowAt
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    // cellForRowAt
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TABLE_VIEW_CELL.HomeCardCell.rawValue, for: indexPath) as? HomeCardCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    // didSelectRowAt
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // todo
+    }
 }
