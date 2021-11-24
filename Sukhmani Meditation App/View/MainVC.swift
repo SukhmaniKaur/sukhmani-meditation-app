ce381d764071df1475c18cab5d7e6ac2582e75cb
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-25 00:08:18 +0530

    designed main vc

---
+++ Sukhmani Meditation App/View/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC.swift
+//  Sukhmani Meditation App
+//
+//  Created by MACBOOK on 25/11/21.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    // OUTLETS
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
