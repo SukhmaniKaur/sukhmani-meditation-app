a3d576d9c397af20ca51a9b720ff0775c373a3b4
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-27 00:40:10 +0530

    data fetched in view model

--- Sukhmani Meditation App/ViewModel/HomeListingViewModel.swift
+++ Sukhmani Meditation App/ViewModel/HomeListingViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SainiUtils
+
+protocol HomeListingDelegate {
+    var collectionArr: Box<[CollectionModel]> { get set }
+    func fetchHomeListing()
+}
+
+struct HomeListingViewModel: HomeListingDelegate {
+    var collectionArr: Box<[CollectionModel]> = Box([CollectionModel]())
+    
+    func fetchHomeListing() {
+        let fsDb = FSDatabase()
+        fsDb.fetchCollection { (response) in
+            if let success = response , !success.isEmpty {
+                self.collectionArr.value = success
+            }
+            else {
+                log.info("ERROR")/
+            }
+        }
+    }
+}
