cd618460b8bd838da3287556ea1aa42cea22bf0e
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-27 00:38:20 +0530

    model added

--- Sukhmani Meditation App/Model/DataModel.swift
+++ Sukhmani Meditation App/Model/DataModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct CollectionModel: Codable {
+    let name: String
+    let doing_right_now: Int
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decodeIfPresent(String.self, forKey: .name) ?? DocumentDefaultValues.Empty.string
+        doing_right_now = try values.decodeIfPresent(Int.self, forKey: .doing_right_now) ?? DocumentDefaultValues.Empty.int
+    }
+    
+    internal init() {
+        self.name = DocumentDefaultValues.Empty.string
+        self.doing_right_now = DocumentDefaultValues.Empty.int
+    }
+}
