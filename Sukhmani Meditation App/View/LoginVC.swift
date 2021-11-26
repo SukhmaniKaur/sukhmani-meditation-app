29720d438ed58a751d7d6206bcc031929fec9d8b
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-27 05:19:00 +0530

    outlets and actions added to google btn

--- Sukhmani Meditation App/View/LoginVC.swift
+++ Sukhmani Meditation App/View/LoginVC.swift
@@ -15,15 +15,9 @@ class LoginVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - signInWithGoogleBtnIsPressed
+    @IBAction func signInWithGoogleBtnIsPressed(_ sender: UIButton) {
+        AppDelegate().sharedDelegate().loginWithGoogle(vc: self)
     }
-    */
 
 }
