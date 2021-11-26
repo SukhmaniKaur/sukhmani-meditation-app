f29522642f358ba69a9b4735afa3b8033b3a043d
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-27 03:44:36 +0530

    fetched docIds

--- Sukhmani Meditation App/ProjectHelper/Database.swift
+++ Sukhmani Meditation App/ProjectHelper/Database.swift
@@ -11,11 +11,12 @@ import FirebaseAuth
 
 class FSDatabase: NSObject {
 
-    func fetchCollection(completion: @escaping (_ dictArr: [CollectionModel]?) -> Void) {
+    func fetchCollection(completion: @escaping (_ dictArr: [CollectionModel]?, _ docId: [String]) -> Void) {
         Firestore.firestore().collection("meditation").getDocuments() { (querySnapshot, err) in
             guard err == nil else { return }
             guard let collectionArr: [CollectionModel] = try? querySnapshot?.toObject() else { return }
-            completion(collectionArr)
+            guard let docArr: [String] = querySnapshot?.getDocID() else { return }
+            completion(collectionArr, docArr)
         }
     }
     
